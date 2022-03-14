82a29f1c189c976f79c80cb337b8f91e311475ed
cu1we1 <cu1we1@163.com>
2022-03-14 20:30:21 +0800

    发布订阅模式

---
+++ src/24.发布订阅者模式/index.js
@@ -0,0 +1,30 @@
+class MySub {
+  constructor() {
+    /**
+     * {
+     *  test: [fn1, fn2]
+     * }
+     *
+     */
+    this.listener = {};
+  }
+  //   订阅
+  on(eventName, fn) {
+    if (!eventName || typeof fn !== "function") return;
+    if (Array.isArray(this.listener[eventName])) {
+      this.listener[eventName].push(fn);
+    } else {
+      this.listener[eventName] = [fn];
+    }
+  }
+  //   发布
+  emit(eventName, ...args) {
+    if (!eventName) return;
+    const _this = this;
+    if (Array.isArray(this.listener[eventName])) {
+      this.listener.forEach(item => {
+        typeof item === "function" && fn.call(_this, args);
+      });
+    }
+  }
+}
