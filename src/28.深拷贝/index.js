7d489b978108e0f4c427fcd74c1b9bd4d0a67150
cuiwei <cuiwei@oppo.com>
2022-04-11 15:04:07 +0800

    深浅拷贝

--- src/28.深拷贝/index.js
+++ src/28.深拷贝/index.js
@@ -6,20 +6,32 @@
  *
  */
 
-function deepClone(originData) {
-  if (typeof originData !== "object") return originData;
+function deepClone(originData, map = new Map()) {
+  if (!(typeof originData === "object" || typeof originData === "function"))
+    return originData;
   if (Object.prototype.toString.call(originData) === "[object Function]") {
-    return originData.bind(this);
+    const resultFn = originData.bind({});
+    for (const key in originData) {
+      if (Object.prototype.hasOwnProperty.call(originData, key)) {
+        resultFn[key] = deepClone(originData[key], map);
+      }
+    }
+    return resultFn;
   } else if (Object.prototype.toString.call(originData) === "[object Object]") {
     const resultObj = {};
     for (let key in originData) {
-      resultObj[key] = deepClone(originData[key]);
+      if (!map.get(key)) {
+        map.set(key, originData[key]);
+        resultObj[key] = deepClone(originData[key], map);
+      } else {
+        resultObj[key] = map.get(key);
+      }
     }
     return resultObj;
   } else if (Object.prototype.toString.call(originData) === "[object Array]") {
     const resultArr = [];
     for (let i = 0, len = originData.length; i < len; i++) {
-      resultArr.push(deepClone(originData[i]));
+      resultArr.push(deepClone(originData[i], map));
     }
     return resultArr;
   } else if (Object.prototype.toString.call(originData) === "[object Null]") {
